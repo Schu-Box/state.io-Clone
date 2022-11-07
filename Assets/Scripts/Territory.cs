9bdcc4a53f808bfa45bebde98542e4985fa7ebef
Dan Schumacher <DanShoes21@gmail.com>
2022-11-07 13:32:49 -0500

    Basic Territory Scripting + Troop Generation

--- Assets/Scripts/Territory.cs
+++ Assets/Scripts/Territory.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Territory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image fillImage;
+    public TextMeshProUGUI troopCounterText;
+
+    public Team teamOccupier;
+
+    public int troops = 10;
+    private int troopMax = 50;
+
+    public float troopIncreaseFrequency = 1f;
+    private float troopIncreaseTimer;
+
+    private void Start()
     {
-        
+        troopIncreaseTimer = troopIncreaseFrequency;
+
+		UpdateTroopCounter();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(troops < troopMax) //If this territory isn't at max troop count
+        {
+			troopIncreaseTimer -= Time.deltaTime;
+			if (troopIncreaseTimer <= 0)
+			{
+                troopIncreaseTimer = troopIncreaseFrequency;
+
+                troops++;
+
+				UpdateTroopCounter();
+			}
+		}
     }
+
+    public void UpdateTroopCounter()
+	{
+		troopCounterText.text = troops.ToString();
+	}
 }
